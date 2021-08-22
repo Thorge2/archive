801bb7b2b98265686c049d2424497433efdf4ab5
Thorge Hemken <thorge@flo.rip>
2021-08-22 20:52:05 +0200

    cleaned some things up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,12 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   // mode: "jit",
-  purge: ["./src/**/*.html", "./src/**/*.jsx"],
+  purge: ["./index.html", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        waves: 'url("/background.svg")',
-      }),
-
       colors: {
         sky: colors.sky,
-        fuchsia: colors.fuchsia,
       },
     },
   },
